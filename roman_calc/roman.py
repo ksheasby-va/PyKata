70bb02835a62d819f423f2f0393ad3ef11a3b8b1
kms123 <sheasby23@me.com>
2016-05-18 11:14:43 -0600

    First commit of code

---
+++ roman_calc/roman.py
@@ -0,0 +1,29 @@
+def parse_numeral(numeral):
+    digits = [parse_digit(digit) for digit in numeral]
+    total = 0
+    i = 0
+    for i in range(0, len(digits) - 1):
+        current_digit = digits[i]
+        next_digit = digits[i + 1]
+        if current_digit < next_digit:
+            total -= current_digit
+        else:
+            total += current_digit
+
+    total += digits[-1]
+
+    return total
+
+
+def parse_digit(character):
+
+    if character == 'I':
+        return 1
+    elif character == 'V':
+        return 5
+    elif character == 'X':
+        return 10
+    elif character == 'L':
+        return 50
+    else:
+        raise NotImplementedError()
---
+++ roman_calc/roman_test.py
@@ -0,0 +1,30 @@
+import unittest
+
+from roman import parse_numeral
+
+
+class RomanNumeralParserTest(unittest.TestCase):
+
+    def test_I_parses_as_one(self):
+        self.assertEqual(1, parse_numeral('I'))
+
+    def test_V_parses_as_five(self):
+        self.assertEqual(5, parse_numeral('V'))
+
+    def test_X_parses_as_ten(self):
+        self.assertEqual(10, parse_numeral('X'))
+
+    def test_II_parses_as_two(self):
+        self.assertEqual(2, parse_numeral('II'))
+
+    def test_III_parses_as_three(self):
+        self.assertEqual(3, parse_numeral('III'))
+
+    def test_IV_parses_as_four(self):
+        self.assertEqual(4, parse_numeral('IV'))
+
+    def test_XXXVIII_parses_as_38(self):
+        self.assertEqual(38, parse_numeral('XXXVIII'))
+
+    def test_XIV_parses_as_14(self):
+        self.assertEqual(14, parse_numeral('XIV'))
