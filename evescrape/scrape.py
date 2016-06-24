8a2b9a1bdeca7b5ca8df01f261f2420991fa7c4c
ksheasby <ksheasby@vendasta.com>
2016-06-24 15:55:57 -0600

    Output 5% to terminal with lables.

--- evescrape/scrape.py
+++ evescrape/scrape.py
@@ -8,22 +8,29 @@ items = {"tritanium": 34,
 	"nocxium": 38,
 }	
 
-class DataCollector():
+inv_items = { v: k for k, v in items.items() }
+
+class DataCollector(object):
 	def load_info(self):
-		url = build_url()
+		url = self.build_url()
 
 		response = requests.get(url)
 
-		data = json.loads(response.content)
+		self.data = json.loads(response.content)
+		for item in self.data:
+			print self.inv_items[item["buy"]["forQuery"]["types"][0]] + ": " + str(item['buy']['fivePercent'])
 
 		
 	def build_url(self):
-		base = "http://api.eve-central.com/api/marketstat/json?"
+		base = "http://api.eve-central.com/api/marketstat/json?regionlimit=10000002&"
 		
-		for key in items.iteritems():	
-			base = base + "typeid=" + items[key] 
-
+		for key in self.items:
+			base = base + "typeid=" + str(self.items[key]) + "&"
+	
 		return base
 
 
+	def __init__(self):
+		self.items = items
+		self.inv_items = inv_items
 
