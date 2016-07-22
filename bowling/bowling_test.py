e8b5fe6ae81a12308acc3e3938acc1742a9c92a3
ksheasby <ksheasby@vendasta.com>
2016-07-22 16:00:01 -0600

    Continue refactor.

--- bowling/bowling_test.py
+++ bowling/bowling_test.py
@@ -20,11 +20,11 @@ class BowlingCalcTests(unittest.TestCase):
     def test_spare_and_open_frame(self):
         self.assertEqual(30, calc_score([3,12,5,2,3]))
 
-    def test_spare_and_open_frame(self):
+    def test_spare_spare_and_open_frame(self):
         self.assertEqual(46, calc_score([2, 13, 8, 7, 3, 2, 0]))
 
-    def test_spare_and_open_frame(self):
-        self.assertEqual(30, calc_score([3, 12, 5, 2, 3]))
+    # def test_spare_and_open_frame(self):
+    #     self.assertEqual(30, calc_score([3, 12, 5, 2, 3]))
 
     def test_strike_strike_open_frame(self):
         self.assertEqual(62, calc_score([15, 15, 2, 3, 5]))
