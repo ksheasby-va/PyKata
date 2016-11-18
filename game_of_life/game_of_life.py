2107992cd58710b3c993cf2e721dfa64d9c38747
Kyle Sheasby <ksheasby@vendasta.com>
2016-11-18 15:41:37 -0600

    Add get_neighbours function.

--- game_of_life/game_of_life.py
+++ game_of_life/game_of_life.py
@@ -4,6 +4,11 @@ def game_of_life(width, height, grid):
 
 
 def make_grid_from_list(width, grid):
+    """
+    Return the string representation of the grid, with proper line breaks.
+    width: integer of width of the grid.
+    grid: list 1-D representation of the grid
+    """
     i = 1
     string = ''
     for cell in grid:
@@ -16,3 +21,19 @@ def make_grid_from_list(width, grid):
             string += cell
 
     return string
+
+
+def get_neighbours(position, width, grid):
+    list_index = position - 1
+    neighbours = []
+    if position > width:
+        neighbours.append(grid[list_index - width - 1])
+        neighbours.append(grid[list_index - width])
+        neighbours.append(grid[list_index - width + 1])
+        neighbours.append(grid[list_index - 1])
+        neighbours.append(grid[list_index + width - 1])
+    neighbours.append(grid[list_index + 1])
+    neighbours.append(grid[list_index + width])
+    neighbours.append(grid[list_index + width + 1])
+
+    return neighbours
