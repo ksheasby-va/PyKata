d9353d3885120bc8d134e48e78116a5fb1cfec92
Kyle Sheasby <ksheasby@vendasta.com>
2016-12-01 12:30:56 -0600

    Write algorithm to calculate distance, passes 2 tests.

--- advent_2016/day_01_1_test.py
+++ advent_2016/day_01_1_test.py
@@ -6,4 +6,11 @@ from day_01_1 import calculate_distance
 class CalculateDistanceTests(unittest.TestCase):
 
     def test_calculate_distance_basic(self):
-        calculate_distance(['L1', 'R1'])
+        result = calculate_distance(['L1', 'R1'])
+
+        self.assertEqual(2, result)
+
+    def test_calculate_distance_3(self):
+        result = calculate_distance(['L1', 'R2'])
+
+        self.assertEqual(3, result)
