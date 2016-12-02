0616ae76c543a939990872f9d8756fab21c72cf0
Kyle Sheasby <ksheasby@vendasta.com>
2016-12-02 08:49:17 -0600

    Day 2: Make mapping and write initial function.

--- advent_2016/day_02_01.py
+++ advent_2016/day_02_01.py
@@ -1,2 +1,9 @@
-def func():
-    pass
+from day_02_01_mapping import MAPPING
+
+
+def get_next_number(directions):
+    current = 5
+    for item in directions:
+        current = MAPPING[current][item]
+
+    return current
--- advent_2016/day_02_01_test.py
+++ advent_2016/day_02_01_test.py
@@ -3,5 +3,5 @@ import unittest
 
 class FuncTests(unittest.TestCase):
 
-    def test_foo(self):
+    def test_up_form_five_gets_2(self):
         pass
