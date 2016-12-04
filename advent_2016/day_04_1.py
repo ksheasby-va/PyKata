35e94126f3d0857a7ef7c47e6ece1af181770e21
Kyle Sheasby <ksheasby@vendasta.com>
2016-12-04 00:44:41 -0600

    Day 4: Implement count_letters and get_five_most_frequent_letters

--- advent_2016/day_04_1.py
+++ advent_2016/day_04_1.py
@@ -1,4 +1,5 @@
 import re
+import string
 
 
 def separate_parts(input_string):
@@ -14,4 +15,21 @@ def separate_parts(input_string):
             checksum = part
         else:
             letters.append(part)
-    return (''.join(letters), sector, checksum)
+    return ''.join(letters), sector, checksum
+
+
+def count_letters(input_string):
+    all_letters = string.ascii_lowercase
+    counts = {}
+    for letter in all_letters:
+        if input_string.count(letter):
+            counts[letter] = input_string.count(letter)
+
+    return counts
+
+
+def get_five_most_frequent_letters(input_dict):
+    """ 5 most frequent letters becomes the checksum """
+    tuple_list = input_dict.items()
+    tuple_list.sort(key=lambda tup: tup[1], reverse=True)
+    return ''.join([i[0] for i in tuple_list[0:5]])
--- advent_2016/day_04_1_test.py
+++ advent_2016/day_04_1_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from day_04_1 import separate_parts
+from day_04_1 import separate_parts, count_letters, get_five_most_frequent_letters
 
 
 class SeparatePartsTests(unittest.TestCase):
@@ -9,4 +9,34 @@ class SeparatePartsTests(unittest.TestCase):
         input = 'aaaaa-bbb-z-y-x-123[abxyz]'
         expected = ('aaaaabbbzyx', '123', '[abxyz]')
         result = separate_parts(input)
+        self.assertEqual(expected, result)
+
+
+class CountLettersTests(unittest.TestCase):
+
+    def test_counts_3_as_in_string(self):
+        input_string = 'aaa'
+        expected = {'a': 3}
+        result = count_letters(input_string)
+        self.assertDictEqual(expected, result)
+
+    def test_counts_all_letters_in_string(self):
+        input_string = 'aaabbcccdc'
+        expected = {'a': 3, 'b': 2, 'c': 4, 'd': 1}
+        result = count_letters(input_string)
+        self.assertDictEqual(expected, result)
+
+
+class GetTopFiveMostFrequentLettersTests(unittest.TestCase):
+
+    def test_proper_letters_returned_in_order(self):
+        input_dict = {'a': 7, 'b': 2, 'c': 4, 'd': 1, 'e': 4, 'f': 8, 'g': 1}
+        expected = 'faceb'
+        result = get_five_most_frequent_letters(input_dict)
+        self.assertEqual(expected, result)
+
+    def test_proper_letters_returned_in_order_from_unordered_dict(self):
+        input_dict = {'a': 7, 'b': 2,  'd': 1, 'e': 4, 'f': 8, 'c': 4, 'g': 1}
+        expected = 'faceb'
+        result = get_five_most_frequent_letters(input_dict)
         self.assertEqual(expected, result)
