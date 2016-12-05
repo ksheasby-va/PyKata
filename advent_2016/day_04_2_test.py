300ce38c24c1ba90a5e18354e4f8916d427aab7e
Kyle Sheasby <kyle.sheasby@gmail.com>
2016-12-04 21:23:51 -0600

    Day 4: write run with input function for part 2.

--- advent_2016/day_04_2_test.py
+++ advent_2016/day_04_2_test.py
@@ -1,6 +1,8 @@
 import unittest
 
+from day_04_1 import separate_parts
 from day_04_2 import decrypt_room_name
+from day_04_input import INPUT
 
 
 class DecryptRoomTests(unittest.TestCase):
@@ -26,4 +28,11 @@ class DecryptRoomTests(unittest.TestCase):
         self.assertEqual('very', result)
 
 
+class RunWithInput(unittest.TestCase):
 
+    def test_run_with_input(self):
+        for room in INPUT.splitlines():
+            result_tuple = separate_parts(room)
+            room_name = decrypt_room_name(result_tuple[0], int(result_tuple[1]))
+            if room_name == 'northpoleobjectstorage':
+                print result_tuple[1]
