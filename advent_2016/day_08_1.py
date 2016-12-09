33e27d98f9de0f5d68022c5fbff1fd6cadc6a4da
Kyle Sheasby <kyle.sheasby@gmail.com>
2016-12-08 19:27:47 -0600

    Day 8: Identify command and execute rect command.

--- advent_2016/day_08_1.py
+++ advent_2016/day_08_1.py
@@ -1,6 +1,17 @@
-COMMANDS = {
-    'RECT': 'rect'
-}
-
 def identify_command_type(command):
-    pass
+    if 'rect' in command:
+        return 'rect'
+    elif 'rotate column' in command:
+        return 'rotx'
+    elif 'rotate row' in command:
+        return 'roty'
+
+
+def execute_rect_command(screen, command):
+    x = int(command.split()[1].split('x')[0])
+    y = int(command.split()[1].split('x')[1])
+
+    for i in xrange(y):
+        for j in xrange(x):
+            screen[i][j] = '#'
+    return screen
--- advent_2016/day_08_1_test.py
+++ advent_2016/day_08_1_test.py
@@ -1,12 +1,69 @@
 import unittest
 
-from day_08_1 import identify_command_type, COMMANDS
+from day_08_1 import identify_command_type, execute_rect_command
 
 
 class IdentifyCommandTypeTests(unittest.TestCase):
 
     def test_rect_command_returns_rect(self):
         command = 'rect 3x2'
-        expected = COMMANDS['RECT']
+        expected = 'rect'
         result = identify_command_type(command)
         self.assertEqual(expected, result)
+
+    def test_rotate_x_returns_rotx(self):
+        command = 'rotate column x=A by B'
+        expected = 'rotx'
+        result = identify_command_type(command)
+        self.assertEqual(expected, result)
+
+    def test_rotate_y_returns_roty(self):
+        command = 'rotate row y=A by B'
+        expected = 'roty'
+        result = identify_command_type(command)
+        self.assertEqual(expected, result)
+
+
+class ExecuteRectCommandTests(unittest.TestCase):
+
+    def test_rect_1_x_1_turns_on_top_corner(self):
+        screen = [
+            ['.'] * 5,
+            ['.'] * 5,
+            ['.'] * 5,
+            ['.'] * 5,
+            ['.'] * 5,
+            ['.'] * 5,
+        ]
+        command = 'rect 1x1'
+        screen = execute_rect_command(screen, command)
+        expected = [
+            ['#', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.'],
+        ]
+        self.assertEqual(screen, expected)
+
+    def test_rect_4_x_3_turns_on_top_corner(self):
+        screen = [
+            ['.'] * 5,
+            ['.'] * 5,
+            ['.'] * 5,
+            ['.'] * 5,
+            ['.'] * 5,
+            ['.'] * 5,
+        ]
+        command = 'rect 4x3'
+        screen = execute_rect_command(screen, command)
+        expected = [
+            ['#', '#', '#', '#', '.'],
+            ['#', '#', '#', '#', '.'],
+            ['#', '#', '#', '#', '.'],
+            ['.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.'],
+            ['.', '.', '.', '.', '.'],
+        ]
+        self.assertEqual(screen, expected)
