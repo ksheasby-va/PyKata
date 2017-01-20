4c17939e71363f4050af30b3a365ca0d88b43bc9
Kyle Sheasby <ksheasby@vendasta.com>
2017-01-20 14:31:57 -0600

    Refactor stuff into more logical places.

--- tourOfHeroes/app/app.module.ts
+++ tourOfHeroes/app/app.module.ts
@@ -1,40 +1,19 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent }  from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard.component';
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule,
-    RouterModule.forRoot([
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      }
-    ])
-  ],
+  imports: [ BrowserModule, FormsModule, AppRoutingModule ],
   declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent ],
   providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
 
-export class AppModule {
-}
+export class AppModule { }
