35c62305586d18cb71b7278071193cdc6c399088
Kyle Sheasby <ksheasby@vendasta.com>
2017-01-20 15:36:01 -0600

    Lots of styling.

--- tourOfHeroes/app/heroes.component.ts
+++ tourOfHeroes/app/heroes.component.ts
@@ -2,9 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
+import { Router } from '@angular/router';
 
 
 @Component({
+  moduleId: module.id,
   selector: 'my-heroes',
   templateUrl: 'heroes.component.html',
   styleUrls: ['heroes.component.css']
@@ -15,7 +17,9 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) { }
+  constructor(
+      private router: Router,
+      private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -27,5 +31,9 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  goToDetail(): void {
+      this.router.navigate(['/detail', this.selectedHero.id]);
   }
 }
