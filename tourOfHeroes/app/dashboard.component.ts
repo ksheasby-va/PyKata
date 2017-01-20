472b4519264349c812a474b89e2a81c404fee290
Kyle Sheasby <kyle.sheasby@gmail.com>
2017-01-19 20:49:04 -0600

    More routing, clicking links opens different components.

--- tourOfHeroes/app/dashboard.component.ts
+++ tourOfHeroes/app/dashboard.component.ts
@@ -9,7 +9,7 @@ import { HeroService } from './hero.service';
   templateUrl: 'dashboard.component.html'
 })
 
-export class DashboardComponent implements OnInit{
+export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
 
   constructor(private heroService: HeroService) { }
