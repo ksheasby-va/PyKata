d769577ce232183c2bdfbb0b209bc801755e787d
Kyle Sheasby <ksheasby@vendasta.com>
2017-04-11 20:37:55 -0600

    Add article comonent and model, so show a single hardcoded article below the form.
    To start of section called `Storing Multiple Articles`

--- book_exercises/angular-reddit/src/app/article/article.component.ts
+++ book_exercises/angular-reddit/src/app/article/article.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, HostBinding} from '@angular/core';
+
+import { Article } from './article.model'
 
 @Component({
   selector: 'app-article',
@@ -6,8 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row';
+  article: Article;
 
-  constructor() { }
+  constructor() {
+    this.article = new Article(
+      'Angular 2',
+      'http://angular.io',
+      10
+    );
+  }
+
+  voteUp(): boolean {
+    this.article.voteUp();
+    return false;
+  }
+
+  voteDown(): boolean {
+    this.article.voteDown();
+    return false;
+  }
 
   ngOnInit() {
   }
