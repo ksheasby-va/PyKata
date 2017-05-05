602046c240c5b4b27f039862c7e5912f6b04ab4b
Kyle Sheasby <ksheasby@vendasta.com>
2017-05-05 15:49:21 -0600

    Write product-image component ts.

--- book_exercises/inventory-app/src/app/product-image/product-image.component.ts
+++ book_exercises/inventory-app/src/app/product-image/product-image.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, HostBinding } from '@angular/core';
+import { Product } from "app/product.model";
 
 @Component({
-  selector: 'app-product-image',
-  templateUrl: './product-image.component.html',
-  styleUrls: ['./product-image.component.css']
+  selector: 'product-image',
+  template: `
+  <img class="product-image" [src]="product.imageUrl"
+  `
 })
-export class ProductImageComponent implements OnInit {
+export class ProductImageComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  @Input() product: Product;
+  @HostBinding('attr.class') cssClass = 'ui small image';
 
 }
