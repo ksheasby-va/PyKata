e93e1ce36a920fe542fcc1fcb5250ca8dbd5fe8e
Kyle Sheasby <kyle.sheasby@gmail.com>
2018-12-09 17:14:37 -0600

    Advent 2018 day 1 part 1

---
+++ advent_2018/day_01/part_1_test.py
@@ -0,0 +1,17 @@
+import unittest
+
+from day_01.part_1 import calculate_frequency
+
+
+class DayOneAdditionTests(unittest.TestCase):
+    def test_first_example_results_in_3(self):
+        example = "+1, +1, +1"
+        self.assertEqual(3, calculate_frequency(example))
+
+    def test_second_example_results_in_0(self):
+        example = "+1, +1, -2"
+        self.assertEqual(0, calculate_frequency(example))
+
+    def test_third_example_results_in_minus_6(self):
+        example = "-1, -2, -3"
+        self.assertEqual(-6, calculate_frequency(example))
